b00c2d1e03acfe45875b455a2c8794687984c39b
Erik Langenborg <erik.langenborg@enverus.com>
2022-03-30 18:14:44 -0400

    Remove purge api refs for safety

--- conversation.sh
+++ conversation.sh
@@ -82,32 +82,6 @@ for i in $@; do
         echo "$t - $i : $output .. invalid json. re-trying..."
       else
         echo "$t - $i : $output .. ok"
-        if [[ $cleanup_matched -gt 0 ]]; then # delete the messages on this channel
-          c_channel=$i
-          touch log/$team_name/$t/$c_channel/purge.done
-          for c_ts in $(jq -r '.messages[].ts' messages/$team_name/$t/$i/$output.json | sort -r); do
-            if [[ $(grep -c "^$c_ts$" log/$team_name/$t/$c_channel/purge.done) -gt 0 ]]; then
-              continue
-            fi
-            x_ts=$(gdate +%s.%3N)
-            boundary='---------------------------'$(generate-digits 29)
-
-            while [[ 1 ]]; do
-              make-request "https://$team_name.slack.com/api/chat.delete?_x_id=$x_id-$x_ts&slack_route=$team_id&_x_version_ts=$x_version_ts" \
-                 -H 'Content-Type: multipart/form-data; boundary='$boundary \
-                 --data-binary $'--'$boundary$'\r\nContent-Disposition: form-data; name="channel"\r\n\r\n'$c_channel$'\r\n--'$boundary$'\r\nContent-Disposition: form-data; name="ts"\r\n\r\n'$c_ts$'\r\n--'$boundary$'\r\nContent-Disposition: form-data; name="token"\r\n\r\n'$token$'\r\n--'$boundary$'\r\nContent-Disposition: form-data; name="_x_reason"\r\n\r\nanimateAndDeleteMessageApi\r\n--'$boundary$'\r\nContent-Disposition: form-data; name="_x_mode"\r\n\r\nonline\r\n--'$boundary$'\r\nContent-Disposition: form-data; name="_x_sonic"\r\n\r\ntrue\r\n--'$boundary$'--\r\n' \
-                 >messages/$team_name/$t/$i/purge.json 2>log/$team_name/$t/$i/purge.log
-
-              status_code=$(cat log/$team_name/$t/$i/purge.log | grep "^< HTTP/" | awk '{ print $3 }')
-              if [[ $status_code -eq 200 ]]; then
-                echo "$c_ts" >> log/$team_name/$t/$i/purge.done
-                break
-              else
-                sleep 1
-              fi
-            done
-          done
-        fi
         if [[ $download_files -gt 0 ]]; then
           for a in $(jq -r '.messages[]|select(.files!=null)|.files[]|select(.url_private_download!=null)|.url_private_download' messages/$team_name/$t/$i/$output.json); do
             p=$(echo $a | awk -F'slack.com/files-pri/' '{ print $2 }')
